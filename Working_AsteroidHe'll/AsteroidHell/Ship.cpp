cfdca7bb56af7b71f6224b2d59f417dd7adc4576
Mitchell Steffens <mdsteffens@live.com>
2018-11-27 16:15:44 -0500

    Ship Stuff

--- Working_AsteroidHe'll/AsteroidHell/Ship.cpp
+++ Working_AsteroidHe'll/AsteroidHell/Ship.cpp
@@ -4,9 +4,58 @@
 
 Ship::Ship()
 {
+	m_fAngle = 0.0f;
+	m_fSpeed = 0.0f;
+	m_v3Position = vector3(0.0f);
 }
 
+Ship::Ship(vector3 a_v3Pos) {
+	m_fAngle = 0.0f;
+	m_fSpeed = 0.0f;
+	m_v3Position = a_v3Pos;
+}
 
 Ship::~Ship()
 {
+}
+
+void Ship::Update() {
+	//update forward vector based on m_fAngle
+	m_v3Position.z = 0.0f; //keep ship on the XY plane
+	m4Rotate *= glm::rotate(float(m_fAngle*PI/180.0f), vector3(0.0f, 0.0f, 1.0f));
+}
+
+void Ship::Display() {
+	//get projection matrix
+	//get view matrix
+	//matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
+	//matrix4 m4View = m_pCameraMngr->GetViewMatrix();
+
+	//apply render list
+	//m_pMesh->Render(m4Projection, m4View, m4Translate * m4Rotate);
+}
+
+void Ship::MoveForward() {
+	m_fSpeed += SPEED_INCREMENT;
+	m_v3Position += (m_v3Forward * m_fSpeed);
+}
+
+void Ship::MoveBackward() {
+	m_v3Position -= (m_v3Forward * m_fSpeed);
+}
+
+void Ship::TurnRight() {
+	m_fAngle += ANGLE_INCREMENT;
+
+	//check radians vs degrees
+	//update forward vector
+	m_v3Forward = vector3(glm::acos(m_fAngle), glm::asin(m_fAngle), 0.0f);
+}
+
+void Ship::TurnLeft() {
+	m_fAngle -= ANGLE_INCREMENT;
+
+	//check radians vs degrees
+	//update forward vector
+	m_v3Forward = vector3(glm::acos(m_fAngle), glm::asin(m_fAngle), 0.0f);
 }
--- Working_AsteroidHe'll/AsteroidHell/Ship.h
+++ Working_AsteroidHe'll/AsteroidHell/Ship.h
@@ -6,17 +6,30 @@ using namespace Simplex;
 class Ship
 {
 	float m_fSpeed = 0.0f; //speed the ship is moving
-	vector3 m_v3Direction = vector3(0.0f); //a normalized vector that stores the direction of travel
 	vector3 m_v3Position = vector3(0.0f); //the position of the ship in space
+	vector3 m_v3Forward = vector3(0.0f); //normalized vector to store the direction
+	float m_fAngle = 0.0f;
+	const float ANGLE_INCREMENT = 1.0f;
+	const float MAX_SPEED = 5.0f;
+	const float SPEED_INCREMENT = 1.0f;
+	matrix4 m4Translate = matrix4(0.0f);
+	matrix4 m4Rotate = matrix4(0.0f);
 	//will need some sort of sprite member
 	//needs a rigidbody member
 public:
 	Ship();
+	Ship(vector3 a_fPos);
 	//parameterized constuctor needed
 	//need update
-	//need display\
-	//need methods that can be called from appclass.cpp that move the ship
-	//need a method to destory the ship
+	void Update();
+	//need display
+	void Display();
+	//need methods that can be called from appclass.cpp to move the ship
+	//need a method to destroy the ship
 	~Ship();
+	void MoveForward();
+	void MoveBackward();
+	void TurnRight();
+	void TurnLeft();
 };
 
